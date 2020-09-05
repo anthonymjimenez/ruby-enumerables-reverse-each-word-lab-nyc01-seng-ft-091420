c1f4a165bb627bcd668cd9741dc20a8e5c614af0
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-05 02:53:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,14 @@ def reverse_each_word(str)
   wordArray = str.split(" ")
   tempArray = []
   finalArray = []
-  wordArray.each do |elm|
+  wordArray.collect_with_index do |elm, index|
     word = elm.split('')
     word.each_with_index do |elm, index|
       tempArray.push(word[word.length - (index + 1)])
     end
-    tempArray.push(" ")
+    if index !== wordArray.length - 1
+      tempArray.push(" ")
+    end
     finalArray.push(tempArray)
     tempArray = []
   end
